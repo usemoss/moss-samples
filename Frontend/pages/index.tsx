450762238ee4f46d0c82bbadae1fffdb76a2bc67
yatharth <yt@usemoss.dev>
2025-10-20 18:28:27 -0500

    Removed contact form

--- Frontend/pages/index.tsx
+++ Frontend/pages/index.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
-import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,7 +14,6 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
-      <Contact />
       <Footer />
     </main>
   )
