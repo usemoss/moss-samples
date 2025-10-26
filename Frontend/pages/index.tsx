2021447c21d2b872a6f50e51e047d0a6ab6e42fa
yatharth <yt@usemoss.dev>
2025-10-26 13:07:22 -0500

    Remove Projects component from index page to streamline layout; no longer needed after recent updates to showcase sample use cases.

--- Frontend/pages/index.tsx
+++ Frontend/pages/index.tsx
@@ -2,7 +2,6 @@ import { Inter } from 'next/font/google'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
-import Projects from './components/Projects'
 import LivekitMossVoiceAgent from './components/sampleusecase/Livekit-Moss-Voice Agent'
 import TalkToYourPdf from './components/sampleusecase/TalkToYourPdf'
 import InpersonaAgent from './components/sampleusecase/InpersonaAgent'
@@ -16,7 +15,6 @@ export default function Home() {
       <Header />
       <Hero />
       <About />
-      <Projects />
       <LivekitMossVoiceAgent />
       <TalkToYourPdf />
       <InpersonaAgent />
